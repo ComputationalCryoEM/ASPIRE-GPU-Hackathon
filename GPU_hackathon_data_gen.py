a73758024e18689eef7f4073793c5a34f4ef7fda
Josh Carmichael <66811911+j-c-c@users.noreply.github.com>
2022-05-24 10:47:48 -0400

    Add files via upload

---
+++ GPU_hackathon_data_gen.py
@@ -0,0 +1,41 @@
+import numpy as np
+from numpy import random
+from numpy.linalg import norm
+
+def J_conjugate(A):
+    """
+    Conjugate the 3x3 matrix A by the diagonal matrix J=diag((-1, -1, 1)).
+
+    :param A: A 3x3 matrix.
+    :return: J*A*J
+    """
+    J = np.diag((-1, -1, 1))
+
+    return J @ A @ J
+
+def buildOuterProducts(n_img):
+    # Build random third rows, ground truth vis (unit vectors)
+    gt_vis = np.zeros((n_img, 3), dtype=np.float32)
+    random.seed(42)
+    for i in range(n_img):
+        v = random.randn(3)
+        gt_vis[i] = v / norm(v)
+
+    # Find outer products viis and vijs for i<j
+    nchoose2 = int(n_img * (n_img - 1) / 2)
+    vijs = np.zeros((nchoose2, 3, 3))
+
+    # All pairs (i,j) where i<j
+    pairs = [(i, j) for i in range(n_img) for j in range(n_img) if i < j]
+
+    for k, (i, j) in enumerate(pairs):
+        vijs[k] = np.outer(gt_vis[i], gt_vis[j])
+
+    # J-conjugate some of these outer products (every other element).
+    vijs_conj = vijs.copy()
+    vijs_conj[::2] = J_conjugate(vijs_conj[::2])
+
+    np.save(f"vijs_conj_n{n_img}.npy", vijs_conj)
+ 
+
+buildOuterProducts(5)
---
+++ GPU_hackathon_power_method.py
@@ -0,0 +1,186 @@
+import numpy as np
+from numpy.linalg import norm
+from numpy import random
+
+#####################
+# Utility Functions #
+#####################
+
+def all_pairs(n):
+    """
+    All pairs indexing (i,j) for i<j.
+
+    :param n: The number of items to be indexed.
+    :return: All n-choose-2 pairs (i,j), i<j.
+    """
+    pairs = [(i, j) for i in range(n) for j in range(n) if i < j]
+
+    return pairs
+
+
+def all_triplets(n):
+    """
+    All 3-tuples (i,j,k) where i<j<k.
+
+    :param n: The number of items to be indexed.
+    :returns: All 3-tuples (i,j,k), i<j<k.
+    """
+    triplets = [
+        (i, j, k) for i in range(n) for j in range(n) for k in range(n) if i < j < k
+    ]
+
+    return triplets
+
+
+def J_conjugate(A):
+    """
+    Conjugate the 3x3 matrix A by the diagonal matrix J=diag((-1, -1, 1)).
+
+    :param A: A 3x3 matrix.
+    :return: J*A*J
+    """
+    J = np.diag((-1, -1, 1))
+
+    return J @ A @ J
+
+
+################
+# Power Method #
+################
+
+def signs_times_v(vijs, vec):
+    """
+    Multiplication of the J-synchronization matrix by a candidate eigenvector.
+
+    The J-synchronization matrix is a matrix representation of the handedness graph, Gamma, whose set of
+    nodes consists of the estimates vijs and whose set of edges consists of the undirected edges between
+    all triplets of estimates vij, vjk, and vik, where i<j<k. The weight of an edge is set to +1 if its
+    incident nodes agree in handednes and -1 if not.
+
+    The J-synchronization matrix is of size (n-choose-2)x(n-choose-2), where each entry corresponds to
+    the relative handedness of vij and vjk. The entry (ij, jk), where ij and jk are retrieved from the
+    all_pairs indexing, is 1 if vij and vjk are of the same handedness and -1 if not. All other entries
+    (ij, kl) hold a zero.
+
+    Due to the large size of the J-synchronization matrix we construct it on the fly as follows.
+    For each triplet of outer products vij, vjk, and vik, the associated elements of the J-synchronization
+    matrix are populated with +1 or -1 and multiplied by the corresponding elements of
+    the current candidate eigenvector supplied by the power method. The new candidate eigenvector
+    is updated for each triplet.
+
+    :param vijs: (n-choose-2)x3x3 array, where each 3x3 slice holds the outer product of vi and vj.
+
+    :param vec: The current candidate eigenvector of length n-choose-2 from the power method.
+
+    :return: New candidate eigenvector of length n-choose-2. The product of the J-sync matrix and vec.
+    """
+
+    # All pairs (i,j) and triplets (i,j,k) where i<j<k
+    n_img = int((1+np.sqrt(1+8*len(vijs)))/2)  # Extract number of images from vijs.
+    pairs = all_pairs(n_img)
+    triplets = all_triplets(n_img)
+
+    # There are 4 possible configurations of relative handedness for each triplet (vij, vjk, vik).
+    # 'conjugate' expresses which node of the triplet must be conjugated (True) to achieve synchronization.
+    conjugate = np.empty((4, 3), bool)
+    conjugate[0] = [False, False, False]
+    conjugate[1] = [True, False, False]
+    conjugate[2] = [False, True, False]
+    conjugate[3] = [False, False, True]
+
+    # 'edges' corresponds to whether conjugation agrees between the pairs (vij, vjk), (vjk, vik),
+    # and (vik, vij). True if the pairs are in agreement, False otherwise.
+    edges = np.empty((4, 3), bool)
+    edges[:, 0] = conjugate[:, 0] == conjugate[:, 1]
+    edges[:, 1] = conjugate[:, 1] == conjugate[:, 2]
+    edges[:, 2] = conjugate[:, 2] == conjugate[:, 0]
+
+    # The corresponding entries in the J-synchronization matrix are +1 if the pair of nodes agree, -1 if not.
+    edge_signs = np.where(edges, 1, -1)
+
+    # For each triplet of nodes we apply the 4 configurations of conjugation and determine the
+    # relative handedness based on the condition that vij @ vjk - vik = 0 for synchronized nodes.
+    # We then construct the corresponding entries of the J-synchronization matrix with 'edge_signs'
+    # corresponding to the conjugation configuration producing the smallest residual for the above
+    # condition. Finally, we the multiply the 'edge_signs' by the cooresponding entries of 'vec'.
+    v = vijs
+    new_vec = np.zeros_like(vec)
+    for (i, j, k) in triplets:
+        ij = pairs.index((i, j))
+        jk = pairs.index((j, k))
+        ik = pairs.index((i, k))
+        vij, vjk, vik = v[ij], v[jk], v[ik]
+        vij_J = J_conjugate(vij)
+        vjk_J = J_conjugate(vjk)
+        vik_J = J_conjugate(vik)
+
+        conjugated_pairs = np.where(
+            conjugate[..., np.newaxis, np.newaxis],
+            [vij_J, vjk_J, vik_J],
+            [vij, vjk, vik],
+        )
+        residual = np.stack([norm(x @ y - z) for x, y, z in conjugated_pairs])
+
+        min_residual = np.argmin(residual)
+
+        # Assign edge weights
+        s_ij_jk, s_ik_jk, s_ij_ik = edge_signs[min_residual]
+
+        # Update multiplication of signs times vec
+        new_vec[ij] += s_ij_jk * vec[jk] + s_ij_ik * vec[ik]
+        new_vec[jk] += s_ij_jk * vec[ij] + s_ik_jk * vec[ik]
+        new_vec[ik] += s_ij_jk * vec[ij] + s_ik_jk * vec[jk]
+
+    return new_vec
+
+def J_sync_power_method(vijs):
+    """
+    Calculate the leading eigenvector of the J-synchronization matrix
+    using the power method.
+
+    As the J-synchronization matrix is of size (n-choose-2)x(n-choose-2), we
+    use the power method to compute the eigenvalues and eigenvectors,
+    while constructing the matrix on-the-fly.
+
+    :param vijs: (n-choose-2)x3x3 array of estimates of relative orientation matrices.
+
+    :return: An array of length n-choose-2 consisting of 1 or -1, where the sign of the
+    i'th entry indicates whether the i'th relative orientation matrix will be J-conjugated.
+    """
+
+    # Set power method tolerance and maximum iterations.
+    epsilon = 1e-3
+    max_iters = 1000
+    random.seed(42)
+    
+    # Initialize candidate eigenvectors
+    n_vijs = vijs.shape[0]
+    vec = random.randn(n_vijs)
+    vec = vec / norm(vec)
+    residual = 1
+    itr = 0
+
+    # Power method iterations
+    while itr < max_iters and residual > epsilon:
+        itr += 1
+        vec_new = signs_times_v(vijs, vec)
+        vec_new = vec_new / norm(vec_new)
+        residual = norm(vec_new - vec)
+        vec = vec_new
+
+    # We need only the signs of the eigenvector
+    J_sync = np.sign(vec)
+
+    return J_sync
+
+
+vijs = np.load("vijs_conj_n5.npy")
+
+J_sync_vec = J_sync_power_method(vijs)
+
+np.save("J_sync_vec_n5.npy", J_sync_vec)             
+             
+
+
+
+
